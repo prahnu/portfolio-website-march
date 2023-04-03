3edffcdc7e98523820b26d1dc3e2e53c7706f17d
prahnu <prahnu07@gmail.com>
2023-04-03 19:32:11 +0530

    this is the initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const closeBtn = document.querySelector('.close-btn');
+const body = document.querySelector('body');
+const imageBanner = document.querySelectorAll('.image-banner');
+const cellHeading = document.querySelector('.cell.heading');
+function controlBubble(event) {
+	if (event.target.closest('.image-box')) {
+		const targetID = event.target.closest('.image-box').attributes[1].value;
+		const targetHover = document.getElementById(`${targetID}`);
+		const targetImageBox = event.target.closest('.image-box');
+		const position = targetImageBox.getBoundingClientRect();
+		const top = event.clientY - position.top;
+		const left = event.clientX - position.left;
+		targetHover.style.top = `${top - 50}px`;
+		targetHover.style.left = `${left - 50}px`;
+	}
+}
+function checkBoxes() {
+	const triggerBottom = (window.innerHeight / 5) * 4;
+	const headingTop = cellHeading.getBoundingClientRect().top;
+	if (headingTop < triggerBottom) {
+		cellHeading.classList.add('show');
+	} else {
+		cellHeading.classList.remove('show');
+	}
+	imageBanner.forEach((image) => {
+		const boxTop = image.getBoundingClientRect().top;
+		if (boxTop < triggerBottom) {
+			image.classList.add('slide');
+		} else {
+			image.classList.remove('slide');
+		}
+	});
+}
+
+closeBtn.addEventListener('click', () => {
+	body.classList.toggle('active');
+});
+
+document.addEventListener('mousemove', controlBubble);
+
+checkBoxes();
+window.addEventListener('scroll', checkBoxes);
---
+++ readme.txt
@@ -0,0 +1,20 @@
+
+COMMANDS TO RUN:
+
+1. To run live server: "live-server"
+2. To detect change in file and compile sass to css: "npm run compile:sass"
+
+
+Note: 
+1. To initiate npm: "npm init".
+2. To install sass-package: "npm install node-sass --save-dev".
+3. Live server is globally installed or if we want to install live-server than run command: "npm i live-server -g".
+
+
+Git hub commands 
+1. git init
+2. git config --global user.name USERNAME
+3. git config --global user.email USEREMAIL
+4. git status
+5. git add -A
+6. 
